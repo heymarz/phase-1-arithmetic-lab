c3120d5bbc9d9d3b6b610b42eaa349327417acd4
Marlena Tsang <Marlena345@gmail.com>
2021-09-21 18:30:16 -0400

    commit tree 2b89a6cf0a65c534de2a7bf1828aa5a72e348ace

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 12;
+let num4 = 8;
+
+const mod = num3 % num4;
+
+let max = Math.max(0, 1, 5 ,7 , 11, 20);
